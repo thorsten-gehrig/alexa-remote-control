c02b13d40d12616e2231ac745041718cfd8d8be3
Alex <alexander.noack@web.de>
2017-12-20 19:17:22 +0100

    moved get_devlist after check_status

--- alexa_remote_control_plain.sh
+++ alexa_remote_control_plain.sh
@@ -3,7 +3,7 @@
 # Amazon Alexa Remote Control (PLAIN shell)
 #  alex(at)loetzimmer.de
 #
-# 2017-12-20: v0.7de(for updates see http://blog.loetzimmer.de/2017/10/amazon-alexa-hort-auf-die-shell-echo.html)
+# 2017-12-20: v0.7e(for updates see http://blog.loetzimmer.de/2017/10/amazon-alexa-hort-auf-die-shell-echo.html)
 #
 ###
 #
